282f4b57f98a06e7a63e0ee74110b935bc7e5aa1
ilya <ilyavas@uw.edu>
2024-04-01 15:13:23 -0700

    changed row deletion to delete multiple rows proper

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -85,6 +85,7 @@ public class GameBoard : MonoBehaviour
                         }
                     }
                 }
+                row --;
             }
         }
         return numLinesCleared;
